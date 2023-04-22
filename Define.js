697712ffed03c58755871d397918755ed4e60ff7
Abhishek Choudhary <86.abhishekchaudhary@gmail.com>
2023-04-22 10:20:37 +0530

    Basic Definations

---
+++ Define.js
@@ -0,0 +1,95 @@
+//! Q What is Java Script ?
+//? Java script is Scripting language.
+//? It's a synchronous language execute the code step or sequence by sequence.
+//? It's an Interpreter language execute the code line by line.
+//? It's a client side and server side language.
+
+//! Define Variables.
+//? Variables are container used to store DATA or VALUES.
+//? Types: >VAR     >LET        >const
+
+// var: Re-assign / Re-declare / Re-initialization
+// let: Re-assing
+// const: only one time initialization
+
+//! Define Data Types.
+//? DATA TYPE defines which type of value is stored in a variable.
+//? Types: Primitive  and  Non-Primitive.
+
+// Primitive: pre-define(Let,var,const) - Number, String, Boolean, Null, undefined.
+// Non-Primitive user-define(Array,Object,Function).
+
+//? Array ?
+// An Array is a collection of multiple data or multiple data type.
+
+//? Object ?
+// Collection of key:value pair which is called property.
+
+//? Function ?
+// A block of code is called function.
+
+//! Define operators.
+//? Operators are operate oprends.
+//? Types: Arithmatic(+,-,*,**,%,/)     Comparision(==,===,!=,!==,>,<,>=,<=)        Assignment(=)       Logical(&&,||,!)
+
+//! Define control statement.
+//? Controls the execution of code.
+//? Types:  Conditional statement   and     Looping statement.
+
+//conditional statement: if , if else , if else if , switch , Ternary operator.
+//Looping statement: for loop , while , do-while.
+
+//! Function 
+//? A block of code is Function.
+//? Types: Normal function, Anonymous function, Function expression, Arrow function, IIFE function(imideatly invoked function expression).
+
+//! Scoping
+//? scoping says that from where the variable may be accessible in the program and defines the variable range.
+//? Types: Global Scope     and     Local scope: Function scope(var)  and  Blocks scope{const, Let}.
+
+//! Hoisting
+//? Java interpreter moves the variable declaration and function declaration to the top of the code.
+
+//! Closure 
+//? A function inside a function is know as closure.
+//? Closure = lexical environment + Function.
+
+//! Methods of ARRAY.
+//? push(),  pop(),  shift(),  unshift().
+// Call back function: some(), Every(), filter(), map(), forEach().
+
+//! Methods of STRING.
+//? toUpperCase(),  toLowerCase(),  trim(),  trimStart(),  trimEnd().
+
+//! Methods of OBJECT.
+//? object.keys(),  .values(),  .entries(),  .create(),  .assign(),  .hasOwnProperty().
+
+//! Call by value 
+//? means copying a variable value with different Address(reference).
+//? works only in primitive datatypes. (changes are made will not affect to any variable value)
+
+//! Call by Reference.
+//? Means copying the property or elements of a variable with same address.
+//? Works only in Non-primitive data types.
+
+// Shaloow copy: spread operator(...name)  and   assign operator.(target,source)
+// Deep copy: JSON.stringify  and   JSON.parse
+// Lodash : Deepclone.
+
+//! Define Destructuring.
+//? Using the properties and elements of an object or an array as a whole.
+
+//! this - keyword
+//? It returns the current object or window.
+
+//! Use strict
+//? It is like a reminder that show error while making any of the mistake in code.Like - missing data-type declaration in a variable.
+
+//! Call 
+//? It calls the one object property with in another object by passing arguments also. It works with the function only.
+
+//! Type
+//? In type work is same as Call but arguments are passed in Array format.
+
+//! Bind 
+//? Function is binded and we can call it anywhere at any time.
