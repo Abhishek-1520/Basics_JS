e3641f41b238ea1a78e9c1bfe7079a4f489d6de5
Abhishek Choudhary <86.abhishekchaudhary@gmail.com>
2023-04-23 11:39:16 +0530

    String concept

---
+++ String.js
@@ -0,0 +1,73 @@
+//! String
+
+//const str = 'Hello today is sunday';
+
+//const str = "Hello is'nt well today";
+
+//const str = 'Hello don\'t want it '
+
+//? Template literal : bapticks
+/* 
+const fname = 'Abhishek';
+const lname = 'Choudhary';
+const add =  'Ghaziabad';
+
+console.log(`Hello, My first name is ${fname}, My surname is ${lname} I'm from ${add}`); */
+
+//? String concatination.
+
+/* const str1 = '86.';
+const str2 = 'abhishekhaudhary';
+const stnum = '@gamil.com';
+
+console.log(str1 + str2 + stnum) */
+
+//! Methods of String.
+
+//? toUpperCase()
+
+/* const str1 = '86.';
+const str2 = 'abhishekhaudhary';
+const stnum = '@gamail.com';
+
+const con=str2.toUpperCase();
+const con2=stnum.toUpperCase();
+
+console.log(str1 + con + con2)
+
+////? toLowerCase()
+
+const lo = con.toLowerCase();
+const lo1 = con2.toLowerCase();
+
+console.log(str1 + lo + lo1); */
+
+//? trim() : To delete start and end string space.
+
+/* const a = '     Abhishek       ';
+console.log(a);
+console.log(a.length);
+
+const tr = a.trim();
+console.log(tr);
+console.log(tr.length); */
+
+//? trimEnd() : To delete space form the end of the string.
+
+/* const a = '     Abhishek       ';
+console.log(a);
+console.log(a.length);
+
+const tr = a.trimEnd();
+console.log(tr);
+console.log(tr.length); */
+
+//? trimStart() : To delete the space from Starting of the string.
+
+/* const a = '     Abhishek       ';
+console.log(a);
+console.log(a.length);
+
+const tr = a.trimStart();
+console.log(tr);
+console.log(tr.length); */
