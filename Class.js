028dc6cebbd54c6ca0df24b3b8ad10a939a95a53
Abhishek <86.abhishekchaudhary@gmail.com>
2023-05-01 09:59:16 +0530

    concept of Class and Inheritence

--- Class.js
+++ Class.js
@@ -16,6 +16,8 @@
 
 /* let obj = new Object();
 
+//* New-Keyword- invokes the cunstructor and create a unique instance(object) at every single time.
+
 obj.Name='Abhishek';
 obj.Class='3rd'
 console.log(obj) */ //{Name: 'Abhishek', Class: '3rd'}
@@ -105,3 +107,117 @@ console.log(myobj); */ // student {} [[Prototype]] :  Object
 /// obj.fun(); //TypeError: obj.fun is not a function
  
 student.fun(); */ // Directly can be called without creating an object.
+
+//? Static function can't access the property of constructor and method function.
+
+//! Class Inheritence.
+//? To inherit all the content of one main class to another sub-class.
+
+
+//* With one construction function in main class only.
+
+/* class employee{
+    constructor(name,salary){
+        console.log(`Name = ${name} and salary = ${salary}`);
+    }
+}
+
+class manager extends employee{
+        //sub-class
+}
+
+let obj = new manager('Abhihsek',9870000); */
+
+//* With constructor function in both class.
+//! Super(): Use to inherit the function from one class to another.
+
+/* class employee{
+    constructor(a,b,c){
+        this.name=a;
+        this.salary=b;
+        console.log(`Name = ${this.name} and salary = ${this.salary} ${c}`);
+    }
+}
+
+class manager extends employee{
+    constructor(a,b,c){
+        super(a,b,c);
+        this.fname = 'Abhishek';
+        this.lname = 'hello';
+        console.log(`First Name = ${this.fname} and Last Nmae = ${this.lname}`);
+        
+    }
+
+}
+
+let obj = new manager('Abhishek',2000000,'All set'); */
+
+//* constructor function and method function with SUPER-Keyword.
+
+/* class employee{
+    constructor(name,surname){
+        this.f_Name = name;
+        this.L_Name = surname;
+        console.log(`First name = ${this.f_Name} and Last name = ${this.L_Name}`);
+    }
+    details(sal,add){
+        this.sal=sal;
+        this.add=add;
+        console.log(`Your name ${this.f_Name} salary ${this.sal} address ${this.add}`);
+    }
+}
+
+class manager extends employee{
+    constructor(name,surname,man_name){
+        super(name,surname);
+        console.log(`Manager name = ${man_name} house name ${this.f_Name}`)
+    }
+    details(sal,add,pa){
+        super.details(sal,add);
+        this.pa = pa;
+        this.tot = this.sal+this.pa;
+        console.log(`Gross salary = ${this.tot} and net salary ${this.sal}`)
+    }
+}
+const object = new manager('Abhishek','Choudhary','Abhishek choudhary');
+object.details(200000,'Ghaziabd',20000); */
+
+/* class employee{
+    constructor(name,add){
+            console.log(name,add);
+    }
+    details(sal,pa){
+        console.log(sal,pa,);
+    }
+}
+class manager extends employee{
+    constructor(n,a,mn){
+        super(n,a);
+        console.log(n,mn,a);
+    }
+    details(s,p,tot=s+p){
+        console.log('total salary'+tot);
+        super.details(s,p);
+    }
+}
+const object=new manager('Abhishek','Ghaziabad','choudhary');
+object.details(200000,3000) */
+
+//* Inherit of static function.
+
+/* class employee{
+    static blue(a){
+        console.log('hello employee',a);
+    }
+}
+
+class manager extends employee{
+    static blue(a){
+        console.log('hello manager',a)
+    }
+}
+let obj = new manager();
+manager.blue('hy');
+employee.blue('hi') */
+
+//! No super keyword is usend in Static function .
