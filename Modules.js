b4cac30ed34b7c998b41415336b3c99f2de304d1
Abhishek <86.abhishekchaudhary@gmail.com>
2023-05-18 20:52:32 +0530

    Concept of Modules

--- Modules.js
+++ Modules.js
@@ -1,12 +1,12 @@
 //! Modules are used to import and export data from one file content to another file.
 
-import { student,employee } from "./Class.js";
-import { obj1 } from "./Call_Value_Ref.js";
+// import { student,employee } from "./Class.js";
+// import { obj1 } from "./Call_Value_Ref.js";
 
-const obj = new student();
-obj.grp();
+// const obj = new student();
+// obj.grp();
 
-const objj = new employee('Abhishek',8787);
+// const objj = new employee('Abhishek',8787);
 
-console.log(obj1)
+// console.log(obj1)
 
